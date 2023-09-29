7651b71227fa0dcb244235fac9d08e03313c3d46
Mingyang-Li <mingyangli1@kpmg.co.nz>
2023-09-29 14:32:52 +1300

    fix: use absolute import

--- packages/template-nestjs/src/main.ts
+++ packages/template-nestjs/src/main.ts
@@ -1,8 +1,8 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from '@/modules/app.module';
-import { ConfigService } from '@nestjs/config';
-import { Env } from './common/env.validator';
 import { INestApplication } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { AppModule } from '@/modules/app.module';
+import { Env } from '@/common/env.validator';
 
 async function bootstrap() {
   const app = await NestFactory.create<INestApplication>(AppModule);
