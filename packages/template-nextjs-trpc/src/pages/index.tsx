b9a451ac9564f6cbe4f6676ed806bd293437dd0a
Mingyang-Li <mingyangli1@kpmg.co.nz>
2023-09-29 14:52:02 +1300

    feat: make prettier rule global

--- packages/template-nextjs-trpc/src/pages/index.tsx
+++ packages/template-nextjs-trpc/src/pages/index.tsx
@@ -1,10 +1,10 @@
-import Head from "next/head";
-import Link from "next/link";
+import Head from 'next/head';
+import Link from 'next/link';
 
-import { api } from "@/utils/api";
+import { api } from '@/utils/api';
 
 export default function Home() {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
+  const hello = api.example.hello.useQuery({ text: 'from tRPC' });
 
   return (
     <>
@@ -43,7 +43,7 @@ export default function Home() {
             </Link>
           </div>
           <p className="text-2xl text-white">
-            {hello.data ? hello.data.greeting : "Loading tRPC query..."}
+            {hello.data ? hello.data.greeting : 'Loading tRPC query...'}
           </p>
         </div>
       </main>
