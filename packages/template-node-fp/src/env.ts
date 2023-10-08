c8ebb1d5975cee3b0d75861bd50fc42d30fbb085
Mingyang-Li <mingyangli118@gmail.com>
2023-10-08 17:37:48 +1300

    fix: lint

--- packages/template-node-fp/src/env.ts
+++ packages/template-node-fp/src/env.ts
@@ -1,11 +1,13 @@
-import { createEnv } from "@t3-oss/env-nextjs";
-import { z } from "zod";
+import { createEnv } from '@t3-oss/env-nextjs';
+import { z } from 'zod';
 
 export const env = createEnv({
   server: {
     DATABASE_URL: z.string().default('NONE'),
     PORT: z.number().default(3333),
-    NODE_ENV: z.enum(["development", "test", "production"]).default("development"),
+    NODE_ENV: z
+      .enum(['development', 'test', 'production'])
+      .default('development'),
   },
   runtimeEnv: {
     DATABASE_URL: process.env.DATABASE_URL,
