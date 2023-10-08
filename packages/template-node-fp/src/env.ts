2edbc87137484a049f6133cc70b4e3108240ee84
Mingyang-Li <mingyangli118@gmail.com>
2023-10-08 17:37:12 +1300

    feat: setup env n validators on app startup

---
+++ packages/template-node-fp/src/env.ts
@@ -0,0 +1,15 @@
+import { createEnv } from "@t3-oss/env-nextjs";
+import { z } from "zod";
+
+export const env = createEnv({
+  server: {
+    DATABASE_URL: z.string().default('NONE'),
+    PORT: z.number().default(3333),
+    NODE_ENV: z.enum(["development", "test", "production"]).default("development"),
+  },
+  runtimeEnv: {
+    DATABASE_URL: process.env.DATABASE_URL,
+    PORT: process.env.PORT,
+    NODE_ENV: process.env.NODE_ENV,
+  },
+});
--- packages/template-node-fp/src/server.ts
+++ packages/template-node-fp/src/server.ts
@@ -1,1 +1,18 @@
-console.log(`=== App started ===`);
+import { env } from './env';
+import * as express from 'express';
+import { Request, Response } from 'express';
+
+const app = express();
+const port = env.PORT;
+
+app.use(express.json());
+app.get('/api/health', (req: Request, res: Response) => {
+  const data = 'ok';
+  res.json(data);
+});
+
+app.listen(port, () => {
+  console.log(
+    `Server is running on port ${port}. Access it via http://localhost:${port}/api/health`,
+  );
+});
