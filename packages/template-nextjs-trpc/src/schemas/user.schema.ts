b7a1b46a41b13490bcc96bb43e104f393df18725
Mingyang-Li <mingyangli1@kpmg.co.nz>
2023-10-25 23:09:03 +1300

    feat: password regex validation

--- packages/template-nextjs-trpc/src/schemas/user.schema.ts
+++ packages/template-nextjs-trpc/src/schemas/user.schema.ts
@@ -6,6 +6,13 @@ export const CreateUserArgsSchema = z.object({
     .email('Please provide a valid email'),
   password: z
     .string({ required_error: 'Please provide a password' })
+    .regex(new RegExp(".*[A-Z].*"), "Please include at least 1 uppercase character")
+  .regex(new RegExp(".*[a-z].*"), "Please include at least 1 lowercase character")
+  .regex(new RegExp(".*\\d.*"), "Please include at least 1 number")
+  .regex(
+    new RegExp(".*[`~<>?,./!@#$%^&*()\\-_+=\"'|{}\\[\\];:\\\\].*"),
+    "One special character"
+  )
     .min(8, 'The password must be at least 8 characters long'),
   firstName: z.string({ required_error: 'Please provide first name' }),
   lastName: z.string({ required_error: 'Please provide last name' }),
