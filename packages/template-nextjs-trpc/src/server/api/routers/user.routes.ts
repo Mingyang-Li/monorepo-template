097c7ac491394abcd909d60b819744ddd1e6b083
Mingyang-Li <mingyangli1@kpmg.co.nz>
2023-10-25 22:56:49 +1300

    fix: creation should be mutation

--- packages/template-nextjs-trpc/src/server/api/routers/user.routes.ts
+++ packages/template-nextjs-trpc/src/server/api/routers/user.routes.ts
@@ -1,11 +1,8 @@
-
 import { createTRPCRouter, publicProcedure } from '@/server/api/trpc';
 import { CreateUserArgs } from '@/schemas/user.schema';
 
 export const UserRoutes = createTRPCRouter({
-  create: publicProcedure
-    .input(CreateUserArgs)
-    .query(({ input }) => {
-      return input;
-    }),
+  create: publicProcedure.input(CreateUserArgs).mutation(({ input }) => {
+    return input;
+  }),
 });
