157855a88272a96386fab62e1ad1c5d4ccba6335
Mingyang-Li <mingyangli118@gmail.com>
2023-10-28 16:20:44 +1300

    refactor: clean main.ts

--- packages/template-nestjs/src/main.ts
+++ packages/template-nestjs/src/main.ts
@@ -3,10 +3,8 @@ import { INestApplication } from '@nestjs/common';
 import { AppModule } from '@/modules/app.module';
 import { env } from '@/env';
 
-async function bootstrap() {
+const bootstrap = async () => {
   const app = await NestFactory.create<INestApplication>(AppModule);
-  // const env = app.get<Env>(ConfigService);
-  console.table(env);
   await app.listen(env.PORT);
 }
 
