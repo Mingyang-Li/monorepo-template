ca9a4ae17b01991abb2130cb6685378476371a05
Mingyang-Li <mingyangli118@gmail.com>
2023-10-28 16:18:11 +1300

    chore: format

--- packages/template-nestjs/src/env.ts
+++ packages/template-nestjs/src/env.ts
@@ -2,8 +2,14 @@ import { z } from 'zod';
 require('dotenv').config();
 
 export const EnvSchema = z.object({
-  DATABASE_URL: z.string({ required_error: `Please include DATABASE_URL in your environment variables` }),
-  PORT: z.coerce.number().positive().max(65536, `options.port should be >= 0 and < 65536`).default(3001),
+  DATABASE_URL: z.string({
+    required_error: `Please include DATABASE_URL in your environment variables`,
+  }),
+  PORT: z.coerce
+    .number()
+    .positive()
+    .max(65536, `options.port should be >= 0 and < 65536`)
+    .default(3001),
   NODE_ENV: z
     .enum(['development', 'test', 'production'])
     .default('development'),
