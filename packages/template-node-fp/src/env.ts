4d937551f1e9ce30bb2332ce94f3da02ccba15db
Mingyang-Li <mingyangli118@gmail.com>
2023-10-28 16:04:24 +1300

    refactor: simplify env validation for node app

--- packages/template-node-fp/src/env.ts
+++ packages/template-node-fp/src/env.ts
@@ -1,17 +1,21 @@
-import { createEnv } from '@t3-oss/env-nextjs';
 import { z } from 'zod';
+require('dotenv').config()
 
-export const env = createEnv({
-  server: {
-    DATABASE_URL: z.string().default('NONE'),
-    PORT: z.number().default(3333),
-    NODE_ENV: z
-      .enum(['development', 'test', 'production'])
-      .default('development'),
-  },
-  runtimeEnv: {
-    DATABASE_URL: process.env.DATABASE_URL,
-    PORT: process.env.PORT,
-    NODE_ENV: process.env.NODE_ENV,
-  },
-});
+export const EnvSchema = z.object({
+  DATABASE_URL: z.string().default('NONE'),
+  PORT: z.coerce.number().default(3333),
+  NODE_ENV: z
+    .enum(['development', 'test', 'production'])
+    .default('development'),
+})
+
+export const validateEnv = () => {
+  const parsed = EnvSchema.safeParse(process.env)
+  if (parsed.success === true) {
+    return parsed.data
+  } else {
+    throw parsed.error
+  }
+}
+
+export const env = validateEnv();
