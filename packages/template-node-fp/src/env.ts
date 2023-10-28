6ca659cbc28552cac365ff10fe269d2fa7e138e3
Mingyang-Li <mingyangli118@gmail.com>
2023-10-28 16:05:17 +1300

    chore: remove @t3-oss/env-nextjs, only use zod

--- packages/template-node-fp/src/env.ts
+++ packages/template-node-fp/src/env.ts
@@ -1,5 +1,5 @@
 import { z } from 'zod';
-require('dotenv').config()
+require('dotenv').config();
 
 export const EnvSchema = z.object({
   DATABASE_URL: z.string().default('NONE'),
@@ -7,15 +7,15 @@ export const EnvSchema = z.object({
   NODE_ENV: z
     .enum(['development', 'test', 'production'])
     .default('development'),
-})
+});
 
 export const validateEnv = () => {
-  const parsed = EnvSchema.safeParse(process.env)
+  const parsed = EnvSchema.safeParse(process.env);
   if (parsed.success === true) {
-    return parsed.data
+    return parsed.data;
   } else {
-    throw parsed.error
+    throw parsed.error;
   }
-}
+};
 
 export const env = validateEnv();
