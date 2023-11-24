e012b7b77ce0730f3d0a2e7348d05fa39046308f
Mingyang-Li <mingyangli1@kpmg.co.nz>
2023-11-25 12:39:17 +1300

    feat: remove caret across package.json files, this way we minimise risks of installing incompatible pkgs in non-local environments

--- packages/template-nestjs/src/decorators/role.decorator.ts
+++
@@ -1,5 +0,0 @@
-import { Reflector } from '@nestjs/core';
-
-export const roles = ['admin', 'standard'] as const;
-export type Role = (typeof roles)[number];
-export const Roles = Reflector.createDecorator<Role[]>();
