fb6a5c7331040293c93ff732bb05a520490a9907
Mingyang-Li <mingyangli1@kpmg.co.nz>
2023-12-11 10:14:14 +1300

    chore: remove extra fields across package.json files, add output dir for node-fp app, fixed server.ts config

--- packages/template-node-fp/src/server.ts
+++ packages/template-node-fp/src/server.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import * as express from 'express';
 import * as bodyParser from 'body-parser';
 import { Request, Response } from 'express';
 import { env } from './env';
