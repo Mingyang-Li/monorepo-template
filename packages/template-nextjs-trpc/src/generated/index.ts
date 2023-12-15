316e8322438047dd47cb0f0412a59340f0382a7b
Mingyang-Li <mingyangli1@kpmg.co.nz>
2023-12-15 16:55:13 +1300

    feat: add folder for generated files on next app, enable eslint-ignore on files in /generated folder

---
+++ packages/template-nextjs-trpc/src/generated/index.ts
@@ -0,0 +1,9 @@
+/**
+ * This is where you keep your generated files (if any)
+ *
+ * Reason we have a dedicated folder for them is that
+ * it makes it easier to disable eslint checks on
+ * generated files if they're all within the same folder,
+ * as all you need to do to ignore eslint checks is to add
+ * the folder path to your .eslintignore
+ */
